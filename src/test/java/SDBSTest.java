bf31b6d6de6ccb2512c86c78f9583ebc46df9a87
laowanggit <vipkia@sina.cn>
2016-09-18 21:58:02 +0800

    添加测试用例

--- src/test/java/SDBSTest.java
+++ src/test/java/SDBSTest.java
@@ -1,5 +1,16 @@
+import com.ws.process.SDBSPicPageProcessor;
+import org.apache.log4j.PropertyConfigurator;
+import us.codecraft.webmagic.Spider;
+
 /**
  * Created by laowang on 16-9-18.
  */
 public class SDBSTest {
+    public static void main(String args[]){
+        PropertyConfigurator.configure(ClassLoader.getSystemResourceAsStream("log4j.properties"));
+        Spider.create(new SDBSPicPageProcessor())
+                .addUrl("http://sdbs.db.aist.go.jp/sdbs/cgi-bin/img_disp.cgi?disptype=disp3&amp;imgdir=cds&amp;fname=CDS07852&amp;sdbsno=1")
+                .thread(3)
+                .run();
+    }
 }
