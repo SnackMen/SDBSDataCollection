74b9a0b57377964a6b47e2da72eb6c758d997e57
laowanggit <vipkia@sina.cn>
2016-09-19 16:55:20 +0800

    数据去重，依次查找每行六条数据，如果数据为N，说明没有数据，直接忽略，否则爬取 ,还需要考虑保存数据位置问题

--- src/test/java/SDBSTest.java
+++ src/test/java/SDBSTest.java
@@ -1,3 +1,5 @@
+import com.mongodb.*;
+import com.sun.xml.internal.bind.v2.TODO;
 import com.ws.process.SDBSPicPageProcessor;
 import org.apache.log4j.PropertyConfigurator;
 import us.codecraft.webmagic.Spider;
@@ -6,11 +8,27 @@ import us.codecraft.webmagic.Spider;
  * Created by laowang on 16-9-18.
  */
 public class SDBSTest {
+    private static MongoClient mongoClient = new MongoClient();
+    private static DB db = mongoClient.getDB("mymongo");
+    private static DBCollection collection = null;
     public static void main(String args[]){
-        PropertyConfigurator.configure(ClassLoader.getSystemResourceAsStream("log4j.properties"));
-        Spider.create(new SDBSPicPageProcessor())
-                .addUrl("http://sdbs.db.aist.go.jp/sdbs/cgi-bin/img_disp.cgi?disptype=disp1&amp;imgdir=esr&amp;fname=ESR0981&amp;sdbsno=96")
-                .thread(3)
-                .run();
+        for(int i=1;i<=100;i++){
+
+            collection  = db.getCollection("sdbs_collection");
+            DBCursor object = collection.find(new BasicDBObject("sdbsno",i));
+            if(object.size()>0){
+                //TODO 数据去重，依次查找每行六条数据，如果数据为"N"，说明没有数据，直接忽略，否则爬取
+                //TODO 还需要考虑保存数据位置问题
+            }
+
+
+
+            PropertyConfigurator.configure(ClassLoader.getSystemResourceAsStream("log4j.properties"));
+
+//            Spider.create(new SDBSPicPageProcessor())
+//                    .addUrl("http://sdbs.db.aist.go.jp/sdbs/cgi-bin/img_disp.cgi?disptype=disp1&amp;imgdir=esr&amp;fname=ESR0981&amp;sdbsno=96")
+//                    .thread(3)
+//                    .start();
+        }
     }
 }
